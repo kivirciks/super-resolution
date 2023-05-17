d9b102c04f57aa42c1b05bec76bb9bb4c7cbece9
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-05-17 11:22:41 +0300

    Update wdsr.py

--- wdsr.py
+++ wdsr.py
@@ -1,14 +1,14 @@
-import time
-import tensorflow as tf
 import numpy as np
-from tensorflow.keras.applications.vgg19 import preprocess_input
-from tensorflow.keras.losses import BinaryCrossentropy, MeanAbsoluteError, MeanSquaredError
-from tensorflow.keras.metrics import Mean
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
-from tensorflow.python.data.experimental import AUTOTUNE
+import tensorflow as tf
+from tensorflow.keras.layers import Add, Conv2D, Input, Lambda
+from tensorflow.keras.models import Model
+import tensorflow_addons as tfa
+from tensorflow.python.keras.layers import Add, Conv2D, Input, Lambda
+from tensorflow.python.keras.models import Model
 import os
+from tensorflow.python.data.experimental import AUTOTUNE
 
+# Задание параметров скачивания датасета
 class DIV2K:
     def __init__(self,
                  scale=2,
@@ -17,15 +17,15 @@ class DIV2K:
                  images_dir='.div2k/images',
                  caches_dir='.div2k/caches'):
 
+        # Задание коэффициента "ухудшения" изображения
         self._ntire_2018 = True
-
         _scales = [2, 3, 4, 8]
-
         if scale in _scales:
             self.scale = scale
         else:
             raise ValueError(f'scale must be in ${_scales}')
 
+        # Разбиение на тестовую и валижационную выборки
         if subset == 'train':
             self.image_ids = range(1, 801)
         elif subset == 'valid':
@@ -33,9 +33,12 @@ class DIV2K:
         else:
             raise ValueError("subset must be 'train' or 'valid'")
 
+        # Задание способа интерполяции (используем бикубическую)
         _downgrades_a = ['bicubic', 'unknown']
         _downgrades_b = ['mild', 'difficult']
 
+        # Установка ограничений на коэффициенты ухудшения изображений и 
+        # способ ухудшения (интерполяция)
         if scale == 8 and downgrade != 'bicubic':
             raise ValueError(f'scale 8 only allowed for bicubic downgrade')
 
@@ -60,39 +63,45 @@ class DIV2K:
     def __len__(self):
         return len(self.image_ids)
 
+    # Предосбработка датасета
+    # AUTOTUNE - автоматически подбираемые параметры
     def dataset(self, batch_size=16, repeat_count=None, random_transform=True):
         ds = tf.data.Dataset.zip((self.lr_dataset(), self.hr_dataset()))
+        # Распараллеивание потоков предобраюотки изображений
         if random_transform:
+            # Map проходит по каждому изображению и подбирает параметры
             ds = ds.map(lambda lr, hr: random_crop(lr, hr, scale=self.scale), num_parallel_calls=AUTOTUNE)
+            # Изменение вращения изображения
             ds = ds.map(random_rotate, num_parallel_calls=AUTOTUNE)
+            # Изменение угла поворота изображения
             ds = ds.map(random_flip, num_parallel_calls=AUTOTUNE)
         ds = ds.batch(batch_size)
         ds = ds.repeat(repeat_count)
         ds = ds.prefetch(buffer_size=AUTOTUNE)
         return ds
 
+    # Параметры для High-Resolution части датасета
     def hr_dataset(self):
+        # Если уже не скачано, но скачивается архив
         if not os.path.exists(self._hr_images_dir()):
             download_archive(self._hr_images_archive(), self.images_dir, extract=True)
-
+        # Сохраняется в кэш
         ds = self._images_dataset(self._hr_image_files()).cache(self._hr_cache_file())
-
+        # Если не сохранен индекс изображения, то тоже сохраняется
         if not os.path.exists(self._hr_cache_index()):
             self._populate_cache(ds, self._hr_cache_file())
-
         return ds
 
+    # Параметры для Low-Resolution части датасета (аналогично HR)
     def lr_dataset(self):
         if not os.path.exists(self._lr_images_dir()):
             download_archive(self._lr_images_archive(), self.images_dir, extract=True)
-
         ds = self._images_dataset(self._lr_image_files()).cache(self._lr_cache_file())
-
         if not os.path.exists(self._lr_cache_index()):
             self._populate_cache(ds, self._lr_cache_file())
-
         return ds
 
+    # Сохранение путей по изображений и индексов
     def _hr_cache_file(self):
         return os.path.join(self.caches_dir, f'DIV2K_{self.subset}_HR.cache')
 
@@ -137,41 +146,35 @@ class DIV2K:
         else:
             return f'DIV2K_{self.subset}_LR_{self.downgrade}_X{self.scale}.zip'
 
+    # Используется, когда мы хотим вернуть одно и то же, независимо от вызываемого дочернего класса
     @staticmethod
+    # Перевод PNG-изображения в наборы векторов (через разложение RGB-каналов)
     def _images_dataset(image_files):
         ds = tf.data.Dataset.from_tensor_slices(image_files)
         ds = ds.map(tf.io.read_file)
         ds = ds.map(lambda x: tf.image.decode_png(x, channels=3), num_parallel_calls=AUTOTUNE)
         return ds
 
+    # Сохранение этого в кеше
     @staticmethod
     def _populate_cache(ds, cache_file):
         print(f'Caching decoded images in {cache_file} ...')
         for _ in ds: pass
         print(f'Cached decoded images in {cache_file}.')
 
-
-# -----------------------------------------------------------
-#  Transformations
-# -----------------------------------------------------------
-
-
+# Обрезка изображения
 def random_crop(lr_img, hr_img, hr_crop_size=96, scale=2):
     lr_crop_size = hr_crop_size // scale
     lr_img_shape = tf.shape(lr_img)[:2]
-
     lr_w = tf.random.uniform(shape=(), maxval=lr_img_shape[1] - lr_crop_size + 1, dtype=tf.int32)
     lr_h = tf.random.uniform(shape=(), maxval=lr_img_shape[0] - lr_crop_size + 1, dtype=tf.int32)
-
     hr_w = lr_w * scale
     hr_h = lr_h * scale
-
     lr_img_cropped = lr_img[lr_h:lr_h + lr_crop_size, lr_w:lr_w + lr_crop_size]
     hr_img_cropped = hr_img[hr_h:hr_h + hr_crop_size, hr_w:hr_w + hr_crop_size]
-
     return lr_img_cropped, hr_img_cropped
 
-
+# Поворот изображений
 def random_flip(lr_img, hr_img):
     rn = tf.random.uniform(shape=(), maxval=1)
     return tf.cond(rn < 0.5,
@@ -179,126 +182,24 @@ def random_flip(lr_img, hr_img):
                    lambda: (tf.image.flip_left_right(lr_img),
                             tf.image.flip_left_right(hr_img)))
 
-
+# Вращение изображений
 def random_rotate(lr_img, hr_img):
     rn = tf.random.uniform(shape=(), maxval=4, dtype=tf.int32)
     return tf.image.rot90(lr_img, rn), tf.image.rot90(hr_img, rn)
 
-
-# -----------------------------------------------------------
-#  IO
-# -----------------------------------------------------------
-
-
+# Путь, откуда скачивается датасет
 def download_archive(file, target_dir, extract=True):
     source_url = f'http://data.vision.ee.ethz.ch/cvl/DIV2K/{file}'
     target_dir = os.path.abspath(target_dir)
     tf.keras.utils.get_file(file, source_url, cache_subdir=target_dir, extract=extract)
     os.remove(os.path.join(target_dir, file))
 
-    DIV2K_RGB_MEAN = np.array([0.4488, 0.4371, 0.4040]) * 255
+# Непосредственно получение датасета
+train = DIV2K(scale=4, downgrade='bicubic', subset='train')
+train_ds = train.dataset(batch_size=16, random_transform=True)
 
-def wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block):
-    x_in = Input(shape=(None, None, 3))
-    x = Lambda(normalize)(x_in)
-
-    # main branch
-    m = conv2d_weightnorm(num_filters, 3, padding='same')(x)
-    for i in range(num_res_blocks):
-        m = res_block(m, num_filters, res_block_expansion, kernel_size=3, scaling=res_block_scaling)
-    m = conv2d_weightnorm(3 * scale ** 2, 3, padding='same', name=f'conv2d_main_scale_{scale}')(m)
-    m = Lambda(pixel_shuffle(scale))(m)
-
-    # skip branch
-    s = conv2d_weightnorm(3 * scale ** 2, 5, padding='same', name=f'conv2d_skip_scale_{scale}')(x)
-    s = Lambda(pixel_shuffle(scale))(s)
-
-    x = Add()([m, s])
-    x = Lambda(denormalize)(x)
-
-    return Model(x_in, x, name="wdsr")
-
-
-def res_block_a(x_in, num_filters, expansion, kernel_size, scaling):
-    x = conv2d_weightnorm(num_filters * expansion, kernel_size, padding='same', activation='relu')(x_in)
-    x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
-    if scaling:
-        x = Lambda(lambda t: t * scaling)(x)
-    x = Add()([x_in, x])
-    return x
-
-
-def res_block_b(x_in, num_filters, expansion, kernel_size, scaling):
-    linear = 0.8
-    x = conv2d_weightnorm(num_filters * expansion, 1, padding='same', activation='relu')(x_in)
-    x = conv2d_weightnorm(int(num_filters * linear), 1, padding='same')(x)
-    x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
-    if scaling:
-        x = Lambda(lambda t: t * scaling)(x)
-    x = Add()([x_in, x])
-    return x
-
-def conv2d_weightnorm(filters, kernel_size, padding='same', activation=None, **kwargs):
-    return tfa.layers.WeightNormalization(Conv2D(filters, kernel_size, padding=padding, activation=activation, **kwargs), data_init=False)
-
-def wdsr_b(scale, num_filters=32, num_res_blocks=8, res_block_expansion=6, res_block_scaling=None):
-    return wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block_b)
-
-def resolve_single(model, lr):
-    return resolve(model, tf.expand_dims(lr, axis=0))[0]
-
-def resolve(model, lr_batch):
-    lr_batch = tf.cast(lr_batch, tf.float32)
-    sr_batch = model(lr_batch)
-    sr_batch = tf.clip_by_value(sr_batch, 0, 255)
-    sr_batch = tf.round(sr_batch)
-    sr_batch = tf.cast(sr_batch, tf.uint8)
-    return sr_batch
-
-def evaluate(model, dataset):
-    psnr_values = []
-    for lr, hr in dataset:
-        sr = resolve(model, lr)
-        psnr_value = psnr(hr, sr)[0]
-        psnr_values.append(psnr_value)
-    return tf.reduce_mean(psnr_values)
-
-# ---------------------------------------
-#  Normalization
-# ---------------------------------------
-def normalize(x, rgb_mean=DIV2K_RGB_MEAN):
-    return (x - rgb_mean) / 127.5
-
-
-def denormalize(x, rgb_mean=DIV2K_RGB_MEAN):
-    return x * 127.5 + rgb_mean
-
-
-def normalize_01(x):
-    """Normalizes RGB images to [0, 1]."""
-    return x / 255.0
-
-
-def normalize_m11(x):
-    """Normalizes RGB images to [-1, 1]."""
-    return x / 127.5 - 1
-
-
-def denormalize_m11(x):
-    """Inverse of normalize_m11."""
-    return (x + 1) * 127.5
-
-
-# ---------------------------------------
-#  Metrics
-# ---------------------------------------
-def psnr(x1, x2):
-    return tf.image.psnr(x1, x2, max_val=255)
-
-
-
-def pixel_shuffle(scale):
-    return lambda x: tf.nn.depth_to_space(x, scale)
+# Усреднение RGB каналов
+DIV2K_RGB_MEAN = np.array([0.4488, 0.4371, 0.4040]) * 255
 
 class Trainer:
     def __init__(self,
@@ -389,6 +290,59 @@ class WdsrTrainer(Trainer):
 
     def train(self, train_dataset, valid_dataset, steps=300000, evaluate_every=1000, save_best_only=True):
         super().train(train_dataset, valid_dataset, steps, evaluate_every, save_best_only)
+ 
+
+def wdsr_a(scale, num_filters=32, num_res_blocks=8, res_block_expansion=4, res_block_scaling=None):
+    return wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block_a)
+
+
+def wdsr_b(scale, num_filters=32, num_res_blocks=8, res_block_expansion=6, res_block_scaling=None):
+    return wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block_b)
+
+
+def wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block):
+    x_in = Input(shape=(None, None, 3))
+    x = Lambda(normalize)(x_in)
+
+    # main branch
+    m = conv2d_weightnorm(num_filters, 3, padding='same')(x)
+    for i in range(num_res_blocks):
+        m = res_block(m, num_filters, res_block_expansion, kernel_size=3, scaling=res_block_scaling)
+    m = conv2d_weightnorm(3 * scale ** 2, 3, padding='same', name=f'conv2d_main_scale_{scale}')(m)
+    m = Lambda(pixel_shuffle(scale))(m)
+
+    # skip branch
+    s = conv2d_weightnorm(3 * scale ** 2, 5, padding='same', name=f'conv2d_skip_scale_{scale}')(x)
+    s = Lambda(pixel_shuffle(scale))(s)
+
+    x = Add()([m, s])
+    x = Lambda(denormalize)(x)
+
+    return Model(x_in, x, name="wdsr")
+
+
+def res_block_a(x_in, num_filters, expansion, kernel_size, scaling):
+    x = conv2d_weightnorm(num_filters * expansion, kernel_size, padding='same', activation='relu')(x_in)
+    x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
+    if scaling:
+        x = Lambda(lambda t: t * scaling)(x)
+    x = Add()([x_in, x])
+    return x
+
+
+def res_block_b(x_in, num_filters, expansion, kernel_size, scaling):
+    linear = 0.8
+    x = conv2d_weightnorm(num_filters * expansion, 1, padding='same', activation='relu')(x_in)
+    x = conv2d_weightnorm(int(num_filters * linear), 1, padding='same')(x)
+    x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
+    if scaling:
+        x = Lambda(lambda t: t * scaling)(x)
+    x = Add()([x_in, x])
+    return x
+
+
+def conv2d_weightnorm(filters, kernel_size, padding='same', activation=None, **kwargs):
+    return tfa.layers.WeightNormalization(Conv2D(filters, kernel_size, padding=padding, activation=activation, **kwargs), data_init=False)
 
 # Create a training context for a WDSR B x4 model with 32 
 # residual blocks.
