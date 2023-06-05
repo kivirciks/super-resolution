107e84ed4f863f544a7c9195eac87947eeaf1eef
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-06-05 23:55:54 +0300

    Create test.py

---
+++ Models/test.py
@@ -0,0 +1,383 @@
+# dataset.py
+from os import listdir
+from os.path import join
+
+import torch.utils.data as data
+from PIL import Image
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
+
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('YCbCr')
+    y, _, _ = img.split()
+    return y
+
+
+class DatasetFromFolder(data.Dataset):
+    def __init__(self, image_dir, input_transform=None, target_transform=None):
+        super(DatasetFromFolder, self).__init__()
+        self.image_filenames = [join(image_dir, x) for x in listdir(image_dir) if is_image_file(x)]
+
+        self.input_transform = input_transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        input_image = load_img(self.image_filenames[index])
+        target = input_image.copy()
+        if self.input_transform:
+            input_image = self.input_transform(input_image)
+        if self.target_transform:
+            target = self.target_transform(target)
+
+        return input_image, target
+
+    def __len__(self):
+        return len(self.image_filenames)
+      
+# data.py
+import tarfile
+from os import remove
+from os.path import exists, join, basename
+from six.moves import urllib
+from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize
+
+def download_bsd300(dest="./dataset"):
+    output_image_dir = join(dest, "BSDS300/images")
+
+    if not exists(output_image_dir):
+        url = "http://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz"
+        print("downloading url ", url)
+
+        data = urllib.request.urlopen(url)
+
+        file_path = join(dest, basename(url))
+        with open(file_path, 'wb') as f:
+            f.write(data.read())
+
+        print("Extracting data")
+        with tarfile.open(file_path) as tar:
+            for item in tar:
+                tar.extract(item, dest)
+
+        remove(file_path)
+
+    return output_image_dir
+
+
+def calculate_valid_crop_size(crop_size, upscale_factor):
+    return crop_size - (crop_size % upscale_factor)
+
+
+def input_transform(crop_size, upscale_factor):
+    return Compose([
+        CenterCrop(crop_size),
+        Resize(crop_size // upscale_factor),
+        ToTensor(),
+    ])
+
+
+def target_transform(crop_size):
+    return Compose([
+        CenterCrop(crop_size),
+        ToTensor(),
+    ])
+
+
+def get_training_set(upscale_factor):
+    root_dir = download_bsd300()
+    train_dir = join(root_dir, "train")
+    crop_size = calculate_valid_crop_size(256, upscale_factor)
+
+    return DatasetFromFolder(train_dir,
+                             input_transform=input_transform(crop_size, upscale_factor),
+                             target_transform=target_transform(crop_size))
+
+
+def get_test_set(upscale_factor):
+    root_dir = download_bsd300()
+    test_dir = join(root_dir, "test")
+    crop_size = calculate_valid_crop_size(256, upscale_factor)
+
+    return DatasetFromFolder(test_dir,
+                             input_transform=input_transform(crop_size, upscale_factor),
+                             target_transform=target_transform(crop_size))
+
+  
+# progress_bar
+import sys
+import time
+
+TOTAL_BAR_LENGTH = 80
+LAST_T = time.time()
+BEGIN_T = LAST_T
+
+
+def progress_bar(current, total, msg=None):
+    global LAST_T, BEGIN_T
+    if current == 0:
+        BEGIN_T = time.time()  # Reset for new bar.
+
+    current_len = int(TOTAL_BAR_LENGTH * (current + 1) / total)
+    rest_len = int(TOTAL_BAR_LENGTH - current_len) - 1
+
+    sys.stdout.write(' %d/%d' % (current + 1, total))
+    sys.stdout.write(' [')
+    for i in range(current_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+
+    current_time = time.time()
+    step_time = current_time - LAST_T
+    LAST_T = current_time
+    total_time = current_time - BEGIN_T
+
+    time_used = '  Step: %s' % format_time(step_time)
+    time_used += ' | Tot: %s' % format_time(total_time)
+    if msg:
+        time_used += ' | ' + msg
+
+    msg = time_used
+    sys.stdout.write(msg)
+
+    if current < total - 1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+# return the formatted time
+def format_time(seconds):
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    seconds_final = int(seconds)
+    seconds = seconds - seconds_final
+    millis = int(seconds*1000)
+
+    output = ''
+    time_index = 1
+    if days > 0:
+        output += str(days) + 'D'
+        time_index += 1
+    if hours > 0 and time_index <= 2:
+        output += str(hours) + 'h'
+        time_index += 1
+    if minutes > 0 and time_index <= 2:
+        output += str(minutes) + 'm'
+        time_index += 1
+    if seconds_final > 0 and time_index <= 2:
+        output += str(seconds_final) + 's'
+        time_index += 1
+    if millis > 0 and time_index <= 2:
+        output += str(millis) + 'ms'
+        time_index += 1
+    if output == '':
+        output = '0ms'
+    return output
+  
+  
+# model.py
+import math
+import torch
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self, num_channels, base_channel, upscale_factor, num_residuals):
+        super(Net, self).__init__()
+
+        self.input_conv = nn.Conv2d(num_channels, base_channel, kernel_size=3, stride=1, padding=1)
+
+        resnet_blocks = []
+        for _ in range(num_residuals):
+            resnet_blocks.append(ResnetBlock(base_channel, kernel=3, stride=1, padding=1))
+        self.residual_layers = nn.Sequential(*resnet_blocks)
+
+        self.mid_conv = nn.Conv2d(base_channel, base_channel, kernel_size=3, stride=1, padding=1)
+
+        upscale = []
+        for _ in range(int(math.log2(upscale_factor))):
+            upscale.append(PixelShuffleBlock(base_channel, base_channel, upscale_factor=2))
+        self.upscale_layers = nn.Sequential(*upscale)
+
+        self.output_conv = nn.Conv2d(base_channel, num_channels, kernel_size=3, stride=1, padding=1)
+
+    def weight_init(self, mean=0.0, std=0.02):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, x):
+        x = self.input_conv(x)
+        residual = x
+        x = self.residual_layers(x)
+        x = self.mid_conv(x)
+        x = torch.add(x, residual)
+        x = self.upscale_layers(x)
+        x = self.output_conv(x)
+        return x
+
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(mean, std)
+        if m.bias is not None:
+            m.bias.data.zero_()
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, num_channel, kernel=3, stride=1, padding=1):
+        super(ResnetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(num_channel, num_channel, kernel, stride, padding)
+        self.conv2 = nn.Conv2d(num_channel, num_channel, kernel, stride, padding)
+        self.bn = nn.BatchNorm2d(num_channel)
+        self.activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+        x = self.bn(self.conv1(x))
+        x = self.activation(x)
+        x = self.bn(self.conv2(x))
+        x = torch.add(x, residual)
+        return x
+
+
+class PixelShuffleBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, upscale_factor, kernel=3, stride=1, padding=1):
+        super(PixelShuffleBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channel, out_channel * upscale_factor ** 2, kernel, stride, padding)
+        self.ps = nn.PixelShuffle(upscale_factor)
+
+    def forward(self, x):
+        x = self.ps(self.conv(x))
+        return x
+      
+# trainer.py
+from __future__ import print_function
+from math import log10
+import torch
+import torch.backends.cudnn as cudnn
+
+
+class EDSRTrainer(object):
+    def __init__(self, config, training_loader, testing_loader):
+        super(EDSRTrainer, self).__init__()
+        self.GPU_IN_USE = torch.cuda.is_available()
+        self.device = torch.device('cuda' if self.GPU_IN_USE else 'cpu')
+        self.model = None
+        self.lr = config.lr
+        self.nEpochs = config.nEpochs
+        self.criterion = None
+        self.optimizer = None
+        self.scheduler = None
+        self.seed = config.seed
+        self.upscale_factor = config.upscale_factor
+        self.training_loader = training_loader
+        self.testing_loader = testing_loader
+
+    def build_model(self):
+        self.model = Net(num_channels=1, upscale_factor=self.upscale_factor, base_channel=64, num_residuals=4).to(self.device)
+        self.model.weight_init(mean=0.0, std=0.02)
+        self.criterion = torch.nn.L1Loss()
+        torch.manual_seed(self.seed)
+
+        if self.GPU_IN_USE:
+            torch.cuda.manual_seed(self.seed)
+            cudnn.benchmark = True
+            self.criterion.cuda()
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, betas=(0.9, 0.999), eps=1e-8)
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[50, 75, 100], gamma=0.5)  # lr decay
+
+    def save(self):
+        model_out_path = "EDSR_model_path.pth"
+        torch.save(self.model, model_out_path)
+        print("Checkpoint saved to {}".format(model_out_path))
+
+    def train(self):
+        self.model.train()
+        train_loss = 0
+        for batch_num, (data, target) in enumerate(self.training_loader):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            loss = self.criterion(self.model(data), target)
+            train_loss += loss.item()
+            loss.backward()
+            self.optimizer.step()
+            progress_bar(batch_num, len(self.training_loader), 'Loss: %.4f' % (train_loss / (batch_num + 1)))
+
+        print("    Average Loss: {:.4f}".format(train_loss / len(self.training_loader)))
+
+    def test(self):
+        self.model.eval()
+        avg_psnr = 0
+
+        with torch.no_grad():
+            for batch_num, (data, target) in enumerate(self.testing_loader):
+                data, target = data.to(self.device), target.to(self.device)
+                prediction = self.model(data)
+                mse = self.criterion(prediction, target)
+                psnr = 10 * log10(1 / mse.item())
+                avg_psnr += psnr
+                progress_bar(batch_num, len(self.testing_loader), 'PSNR: %.4f' % (avg_psnr / (batch_num + 1)))
+
+        print("    Average PSNR: {:.4f} dB".format(avg_psnr / len(self.testing_loader)))
+
+    def run(self):
+        self.build_model()
+        for epoch in range(1, self.nEpochs + 1):
+            print("\n===> Epoch {} starts:".format(epoch))
+            self.train()
+            self.test()
+            self.scheduler.step(epoch)
+            if epoch == self.nEpochs:
+                self.save()
+                
+                
+from __future__ import print_function
+import argparse
+from torch.utils.data import DataLoader
+
+# ===========================================================
+# Training settings
+# ===========================================================
+parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
+# hyper-parameters
+parser.add_argument('--batchSize', type=int, default=1, help='training batch size, default = 1')
+parser.add_argument('--testBatchSize', type=int, default=1, help='testing batch size, default = 1')
+parser.add_argument('--nEpochs', type=int, default=20, help='number of epochs to train for, default = 20')
+parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default = 0.01')
+parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default = 123')
+#parser.add_argument("-f", required=False)
+
+# model configuration
+parser.add_argument('--upscale_factor', '-uf',  type=int, default=4, help="super resolution upscale factor")
+parser.add_argument('--model', '-m', type=str, default='model', help='choose which model is going to use')
+
+args = parser.parse_args()
+
+
+def main():
+    # ===========================================================
+    # Set train dataset & test dataset
+    # ===========================================================
+    print('===> Loading datasets')
+    train_set = get_training_set(args.upscale_factor)
+    test_set = get_test_set(args.upscale_factor)
+    training_data_loader = DataLoader(dataset=train_set, batch_size=args.batchSize, shuffle=True)
+    testing_data_loader = DataLoader(dataset=test_set, batch_size=args.testBatchSize, shuffle=False)
+
+    model = EDSRTrainer(args, training_data_loader, testing_data_loader)
+    model.run()
+
+
+if __name__ == '__main__':
+    main()
