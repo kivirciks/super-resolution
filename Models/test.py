c5c7abb7fd4c4e0251fbc779e2c5c2db5af436cb
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-06-06 10:12:15 +0300

    Update test.py

--- Models/test.py
+++ Models/test.py
@@ -250,7 +250,7 @@ class ResnetBlock(nn.Module):
         self.conv1 = nn.Conv2d(num_channel, num_channel, kernel, stride, padding)
         self.conv2 = nn.Conv2d(num_channel, num_channel, kernel, stride, padding)
         self.bn = nn.BatchNorm2d(num_channel)
-        self.activation = nn.Softmin(inplace=True)
+        self.activation = nn.Sigmoid()
         #self.activation = nn.ReLU(inplace=True)
 
     def forward(self, x):
