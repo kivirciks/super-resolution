dd2d1339d84423904c20ee622593dd1e30b0a0b8
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-06-07 01:38:03 +0300

    Update solver.py

--- Models/FSRCNN/solver.py
+++ Models/FSRCNN/solver.py
@@ -34,8 +34,8 @@ class FSRCNNTrainer(object):
             cudnn.benchmark = True
             self.criterion.cuda()
 
-        #self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        #self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[50, 75, 100], gamma=0.5)  # lr decay
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[50, 75, 100], gamma=0.5)  # lr decay
 
     def save_model(self):
         model_out_path = "model_path.pth"
@@ -47,11 +47,11 @@ class FSRCNNTrainer(object):
         train_loss = 0
         for batch_num, (data, target) in enumerate(self.training_loader):
             data, target = data.to(self.device), target.to(self.device)
-            #self.optimizer.zero_grad()
+            self.optimizer.zero_grad()
             loss = self.criterion(self.model(data), target)
             train_loss += loss.item()
             loss.backward()
-            #self.optimizer.step()
+            self.optimizer.step()
             progress_bar(batch_num, len(self.training_loader), 'Loss: %.4f' % (train_loss / (batch_num + 1)))
 
         print("    Average Loss: {:.4f}".format(train_loss / len(self.training_loader)))
@@ -77,6 +77,6 @@ class FSRCNNTrainer(object):
             print("\n===> Epoch {} starts:".format(epoch))
             self.train()
             self.test()
-            #self.scheduler.step(epoch)
+            self.scheduler.step(epoch)
             if epoch == self.nEpochs:
                 self.save_model()
