89c271f5ff60aec2c9514c983aa781ac6cf3677c
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-06-07 10:04:59 +0300

    Update model.py

--- Models/SRGAN/model.py
+++ Models/SRGAN/model.py
@@ -104,8 +104,7 @@ class Discriminator(nn.Module):
         x = swish(self.bn8(self.conv8(x)))
 
         x = self.conv9(x)
-        return nn.Tanh(F.avg_pool2d(x, x.size()[2:])).view(x.size()[0], -1)
-        #return torch.sigmoid(F.avg_pool2d(x, x.size()[2:])).view(x.size()[0], -1)
+        return torch.sigmoid(F.avg_pool2d(x, x.size()[2:])).view(x.size()[0], -1)
 
     def weight_init(self, mean=0.0, std=0.02):
         for m in self._modules:
