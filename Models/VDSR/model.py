5c78530bd83909ae0ff41031550714c61f299275
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-06-08 01:02:30 +0300

    Update model.py

--- Models/VDSR/model.py
+++ Models/VDSR/model.py
@@ -1,21 +1,36 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.utils.prune as prune
+import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self, num_channels, base_channels, num_residuals):
         super(Net, self).__init__()
         #self.input_conv = nn.Sequential(nn.Conv2d(num_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.Sigmoid())
         #self.input_conv = nn.Sequential(nn.Conv2d(num_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.LeakyReLU())
-        self.input_conv = nn.Sequential(nn.Conv2d(num_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ELU())
+        #self.input_conv = nn.Sequential(nn.Conv2d(num_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ELU())
         #self.input_conv = nn.Sequential(nn.Conv2d(num_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.Tanh())
-        #self.input_conv = nn.Sequential(nn.Conv2d(num_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ReLU(inplace=True))        
+        self.input_conv = nn.Sequential(nn.Conv2d(num_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ReLU(inplace=True))  
+        # ===========================================================
+        # Изменения здесь
+        # ===========================================================
+        prune.random_unstructured(self.input_conv, name="weight", amount=0.1)
+        
         #self.residual_layers = nn.Sequential(*[nn.Sequential(nn.Conv2d(base_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.Sigmoid()) for _ in range(num_residuals)])
         #self.residual_layers = nn.Sequential(*[nn.Sequential(nn.Conv2d(base_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.LeakyReLU()) for _ in range(num_residuals)])
-        self.residual_layers = nn.Sequential(*[nn.Sequential(nn.Conv2d(base_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ELU()) for _ in range(num_residuals)])
+        #self.residual_layers = nn.Sequential(*[nn.Sequential(nn.Conv2d(base_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ELU()) for _ in range(num_residuals)])
         #self.residual_layers = nn.Sequential(*[nn.Sequential(nn.Conv2d(base_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.Tanh()) for _ in range(num_residuals)])
-        #self.residual_layers = nn.Sequential(*[nn.Sequential(nn.Conv2d(base_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ReLU(inplace=True)) for _ in range(num_residuals)])
+        self.residual_layers = nn.Sequential(*[nn.Sequential(nn.Conv2d(base_channels, base_channels, kernel_size=3, stride=1, padding=1, bias=False), nn.ReLU(inplace=True)) for _ in range(num_residuals)])
+        # ===========================================================
+        # Изменения здесь
+        # ===========================================================
+        prune.random_unstructured(self.residual_layers, name="weight", amount=0.1)
+        
         self.output_conv = nn.Conv2d(base_channels, num_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        # ===========================================================
+        # Изменения здесь
+        # ===========================================================
+        prune.random_unstructured(self.output_conv, name="weight", amount=0.1)
 
     def weight_init(self):
         for m in self._modules:
