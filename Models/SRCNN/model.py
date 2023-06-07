7101a7839f820c1881320e220cc2ee240da20582
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-06-08 00:57:30 +0300

    Update model.py

--- Models/SRCNN/model.py
+++ Models/SRCNN/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.utils.prune as prune
+import torch.nn.functional as F
 
 class Net(torch.nn.Module):
     def __init__(self, num_channels, base_filter, upscale_factor=2):
@@ -10,21 +11,29 @@ class Net(torch.nn.Module):
             nn.Conv2d(in_channels=num_channels, out_channels=base_filter, kernel_size=9, stride=1, padding=4, bias=True),
             #nn.Sigmoid(),
             #nn.LeakyReLU(),
-            nn.ELU(),
+            #nn.ELU(),
             #nn.Tanh(),
-            #nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=base_filter, out_channels=base_filter // 2, kernel_size=1, bias=True),
             #nn.Sigmoid(),
             #nn.LeakyReLU(),
-            nn.ELU(),
+            #nn.ELU(),
             #nn.Tanh(),
-            #nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=base_filter // 2, out_channels=num_channels * (upscale_factor ** 2), kernel_size=5, stride=1, padding=2, bias=True),
             nn.PixelShuffle(upscale_factor)
         )
+        # ===========================================================
+        # Изменения здесь
+        # ===========================================================
+        prune.random_unstructured(self.layers, name="weight", amount=0.1)
 
     def forward(self, x):
         out = self.layers(x)
+        # ===========================================================
+        # Изменения здесь
+        # ===========================================================
+        prune.random_unstructured(self.layers, name="weight", amount=0.1)
         return out
 
     def weight_init(self, mean, std):
