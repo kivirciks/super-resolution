e346396c489f01b68722499f1f75545a8e3663ae
kivirciks <117316334+kivirciks@users.noreply.github.com>
2023-06-08 14:33:44 +0300

    Update solver.py

--- Models/SubPixelCNN/solver.py
+++ Models/SubPixelCNN/solver.py
@@ -36,7 +36,8 @@ class SubPixelTrainer(object):
             self.criterion.cuda()
 
         #self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        #self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[50, 75, 100], gamma=0.5)  # lr decay
+        self.optimizer = torch.optim.RMSprop(self.model.parameters(), lr=0.01, alpha=0.99, eps=1e-08)
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[50, 75, 100], gamma=0.5)  # lr decay
 
     def save(self):
         model_out_path = "SUB_model_path.pth"
@@ -48,11 +49,11 @@ class SubPixelTrainer(object):
         train_loss = 0
         for batch_num, (data, target) in enumerate(self.training_loader):
             data, target = data.to(self.device), target.to(self.device)
-            #self.optimizer.zero_grad()
+            self.optimizer.zero_grad()
             loss = self.criterion(self.model(data), target)
             train_loss += loss.item()
             loss.backward()
-            #self.optimizer.step()
+            self.optimizer.step()
             progress_bar(batch_num, len(self.training_loader), 'Loss: %.4f' % (train_loss / (batch_num + 1)))
 
         print("    Average Loss: {:.4f}".format(train_loss / len(self.training_loader)))
@@ -78,6 +79,6 @@ class SubPixelTrainer(object):
             print("\n===> Epoch {} starts:".format(epoch))
             self.train()
             self.test()
-            #self.scheduler.step(epoch)
+            self.scheduler.step(epoch)
             if epoch == self.nEpochs:
                 self.save()
